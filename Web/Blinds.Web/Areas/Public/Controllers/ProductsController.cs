e023227f76b2eea3c9ac590f4e3179b756b456af
Malkovski <malkovski.georgi@gmail.com>
2016-02-07 17:39:15 +0200

    Add missing Product Controller

---
+++ Web/Blinds.Web/Areas/Public/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿namespace Blinds.Web.Areas.Public.Controllers
+{
+    using Web.Controllers;
+    using System.Web.Mvc;
+    using Models;
+
+    public class ProductsController : BaseController
+    {
+        public ActionResult Index(int id)
+        {
+            var model = LoadModel<ProductsModel, int>(id);
+            return View(model);
+        }
+    }
+}
